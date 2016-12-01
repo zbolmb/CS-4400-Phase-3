c4b070199a423ddf6f3004ed3b4b953f437a40e5
Zhijian <zhijianli@gatech.edu>
2016-12-01 18:00:03 -0500

    Added admin main page and add project page

--- python/app.py
+++ python/app.py
@@ -126,7 +126,7 @@ class App:
             messagebox.showinfo("Hello", "Welcome to The Database")
             getAdmin = "SELECT admin FROM User WHERE Username = %s AND Password = %s"
             self.cursor.execute(getAdmin, (user, password))
-            admin = self.cursor.fetchone()
+            admin = self.cursor.fetchone()[0]
             self.rootWin.destroy()
             if admin == 1:
                 self.AdminMainPage()
@@ -142,9 +142,10 @@ class App:
         self.root = Tk()
         self.root.wm_title("Main Page")
         self.cursor = self.db.cursor()
+
         def addcatrow():
             print("hello")
-                
+
         # row 1
         mePage = Button(self.root, text='Me', command=self.MePage)
         mePage.grid(row=1, column=0, sticky=W)
@@ -153,46 +154,48 @@ class App:
         Label(self.root, text='Title').grid(row=2, column=0, sticky=W)
         self.eTitle = Entry(self.root)
         self.eTitle.grid(row=2, column=1)
-        Label(self.root, text='Category').grid(row=2, column = 2, sticky = W)
+        Label(self.root, text='Category').grid(row=2, column=2, sticky=W)
         #Categories = self.cursor.execute("SELECT DISTINCT Category FROM Projects, Courses")
         #Categories = self.cursor.fetchall()
-        CatVar=StringVar(self.root)
+        CatVar = StringVar(self.root)
         CatVar.set("Please Select")
         CatDrop = OptionMenu(self.root, CatVar, 'Categories')
-        CatDrop.grid(row = 2, column =3, padx = 1, pady = 1)
-        AddCat = Button(self.root, text = "Add Another Category", fg= 'blue', relief= 'flat', command = addcatrow)
-        AddCat.grid(row = 2, column = 4)
+        CatDrop.grid(row=2, column=3, padx=1, pady=1)
+        AddCat = Button(self.root, text="Add Another Category",
+                        fg='blue', relief='flat', command=addcatrow)
+        AddCat.grid(row=2, column=4)
 
-        Label(self.root, text='Designation').grid(row=3, column = 0, sticky = W)
-        Label(self.root, text='Major').grid(row=4, column = 0, sticky = W)
-        Label(self.root, text='Year').grid(row=5, column = 0, sticky = W)
+        Label(self.root, text='Designation').grid(row=3, column=0, sticky=W)
+        Label(self.root, text='Major').grid(row=4, column=0, sticky=W)
+        Label(self.root, text='Year').grid(row=5, column=0, sticky=W)
         DesVar = StringVar(self.root)
         DesVar.set("Please Select")
         MaVar = StringVar(self.root)
         MaVar.set("Please Select")
         YearVar = StringVar(self.root)
         YearVar.set("Please Select")
-        YearOpt = ["Freshman", "Sophomore", "Junior", "Senior"] 
+        YearOpt = ["Freshman", "Sophomore", "Junior", "Senior"]
         #Designations = self.cursor.execute("SELECT DISTINCT Designation FROM Projects, Courses")
         #Designations = self.cursor.fetchall()
         #Majors = self.cursor.execute("SELECT DISTINCT Major FROM Projects, Courses")
         #Majors = self.cursor.fetchall()
         DesDrop = OptionMenu(self.root, DesVar, 'Designations')
-        DesDrop.grid(row = 3, column =1, padx = 1, pady = 1)
+        DesDrop.grid(row=3, column=1, padx=1, pady=1)
 
         MajDrop = OptionMenu(self.root, MaVar, 'Majors')
-        MajDrop.grid(row = 4, column =1, padx = 1, pady = 1)
+        MajDrop.grid(row=4, column=1, padx=1, pady=1)
 
         YearDrop = OptionMenu(self.root, YearVar, *YearOpt)
-        YearDrop.grid(row = 5, column =1, padx = 1, pady = 1)
+        YearDrop.grid(row=5, column=1, padx=1, pady=1)
 
         ProjCorBo = StringVar(self.root)
         ProjCorBo.set("Both")
-        ProjCorButtonOne = Radiobutton(self.root, text = "Course", variable = ProjCorBo, value = "Course").grid(row = 5, column =3)
-        ProjCorButtonTwo = Radiobutton(self.root, text = "Project", variable = ProjCorBo, value = "Project").grid(row = 5, column =4)
-        ProjCorButtonTwo = Radiobutton(self.root, text = "Both", variable = ProjCorBo, value = "Both").grid(row = 5, column =5)
-
-        
+        ProjCorButtonOne = Radiobutton(
+            self.root, text="Course", variable=ProjCorBo, value="Course").grid(row=5, column=3)
+        ProjCorButtonTwo = Radiobutton(
+            self.root, text="Project", variable=ProjCorBo, value="Project").grid(row=5, column=4)
+        ProjCorButtonTwo = Radiobutton(
+            self.root, text="Both", variable=ProjCorBo, value="Both").grid(row=5, column=5)
 
         def ApplyFilter():
             print("To do")
@@ -203,27 +206,28 @@ class App:
             YearVar.set("Please Select")
             CatVar.set("Please Select")
             ProjCorBo.set("Both")
-        
 
-        ApplyFil = Button(self.root, text = "Apply Filter", command = ApplyFilter)
-        ApplyFil.grid(row = 6, column = 5)
-        ResetFil = Button(self.root, text = "Reset Filter", command = ResetFilter)
-        ResetFil.grid(row = 6, column = 6)
+        ApplyFil = Button(self.root, text="Apply Filter", command=ApplyFilter)
+        ApplyFil.grid(row=6, column=5)
+        ResetFil = Button(self.root, text="Reset Filter", command=ResetFilter)
+        ResetFil.grid(row=6, column=6)
 
-        self.root.mainloop()                                                                
+        self.root.mainloop()
 
     def MePage(self):
         self.cursor.close()
         self.root.destroy()
         self.root = Tk()
         self.root.wm_title("Me")
-        mePage = Button(self.root, text='Edit Profile', command=self.StudEdPro, width = 30, height = 4)
-        mePage.grid(row=1, column=0, sticky=W+E)
-        mePage = Button(self.root, text='View Application', command=self.StuViewApp, width = 30, height = 4)
-        mePage.grid(row=2, column=0, sticky=W+E)
-        mePage = Button(self.root, text='Back', command=self.BacktoMainPage, padx=5, pady=5, width = 30, height = 4)
-        mePage.grid(row=3, column=0, sticky=W+E)
-
+        mePage = Button(self.root, text='Edit Profile',
+                        command=self.StudEdPro, width=30, height=4)
+        mePage.grid(row=1, column=0, sticky=W + E)
+        mePage = Button(self.root, text='View Application',
+                        command=self.StuViewApp, width=30, height=4)
+        mePage.grid(row=2, column=0, sticky=W + E)
+        mePage = Button(self.root, text='Back', command=self.BacktoMainPage,
+                        padx=5, pady=5, width=30, height=4)
+        mePage.grid(row=3, column=0, sticky=W + E)
 
     def BacktoMainPage(self):
         self.root.destroy()
@@ -235,6 +239,74 @@ class App:
     def StuViewApp(self):
         print("to do")
 
+    # AdminMainPage
+    def AdminMainPage(self):
+        self.root = Tk()
+        self.root.wm_title("Admin Main Page")
+        Label(self.root, text='Choose Functionality').grid(row=1, column=1, sticky=W)
+        viewApps = Button(self.root, text='View Applications', command=self.ViewAppsPage)
+        viewApps.grid(row=2, column=1)
+        viewProjReport = Button(self.root, text='View Popular Project Reports', command=self.ViewProjReportPage)
+        viewProjReport.grid(row=3, column=1)
+        viewAppReport = Button(self.root, text='View Application Report', command=self.ViewAppReportPage)
+        viewAppReport.grid(row=4, column=1)
+        addProj = Button(self.root, text='Add a Project', command=self.AddProjPage)
+        addProj.grid(row=5, column=1)
+        addCourse = Button(self.root, text='Add a Course', command=self.AddCoursePage)
+        addCourse.grid(row=6, column=1)
+
+        logout = Button(self.root, text='Logout', command=self.Logout)
+        logout.grid(row=7, column=2)
+
+        self.root.mainloop()
+
+    # Chris Lung
+    def ViewAppsPage(self):
+        print('TODO')
+
+    # Chris Lung
+    def ViewProjReportPage(self):
+        print('TODO')
+
+    # Chris Lung
+    def ViewAppReportPage(self):
+        print('TODO')
+
+    def AddProjPage(self):
+        self.root.destroy()
+        self.root = Tk()
+        self.root.wm_title("Add Project Page")
+
+        Label(self.root, text="Add a Project").grid(row=1, column=1, sticky=W + E)
+        # row 2
+        Label(self.root, text="Project Name:").grid(row=2, column=0)
+        self.projName = Entry(self.root)
+        self.projName.grid(row=2, column=1)
+        # row 3
+        Label(self.root, text="Advisor:").grid(row=3, column=0)
+        self.advisorName = Entry(self.root)
+        self.advisorName.grid(row=3, column=1)
+        # row 4
+        Label(self.root, text="Advisor Email:").grid(row=4, column=0)
+        self.advisorEmail = Entry(self.root)
+        self.advisorEmail.grid(row=4, column=1)
+        # row 5
+        Label(self.root, text="Description").grid(row=5, column=0)
+        self.description = Text(self.root, height=10, width=25)
+        self.description.grid(row=5, column=1)
+        # row 6
+        # change 'Designations' to what Dennis has
+        Label(self.root, text="Category").grid(row=6, column=0)
+        DesDrop = OptionMenu(self.root, DesVar, 'Designations')
+        DesDrop.grid(row=6, column=1, padx=1, pady=1)
+
+        self.root.mainloop()
+
+    def AddCoursePage(self):
+        print('TODO')
+
+    def Logout(self):
+        print('TODO')
 
     def Connect(self):
         try:
